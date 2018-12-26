185c3a27dd4368705513f6d1419892e4da8bc9a6
dpoliszak <dawidpoliszak@op.pl>
2018-12-26 12:11:57 +0100

    Configure prettier

--- test/integration/models/topic.model.js
+++ test/integration/models/topic.model.js
@@ -5,20 +5,19 @@ chai.use(require('chai-like'));
 chai.use(require('chai-things')); // Don't swap these two
 
 describe('Topic (model)', () => {
-  beforeEach((done) => {
-    return Topic.create({test: 'test'}).then(
-      done()
-    );
+  beforeEach(done => {
+    return Topic.create({ test: 'test' }).then(done());
   });
   describe('get all Topics', () => {
     it('should get all topics', done => {
       Topic.find()
         .then(topics => {
-          expect(topics).to.be.an('array').that.contains.something.like({test: 'test'});
+          expect(topics)
+            .to.be.an('array')
+            .that.contains.something.like({ test: 'test' });
           done();
         })
         .catch(done);
     });
   });
-
 });
